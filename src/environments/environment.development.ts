4220ac43a2b5dfa319e20d94dd880e2f996718fc
Dhruv <dsindha@argusoft.com>
2023-04-06 15:11:23 +0530

    Auto-generated commit [ci skip]

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  firebaseAPIKey: 'AIzaSyB9YNikK_nFhURlOxMGN-f2CiqpYx9_2is'
-};
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseAPIKey: 'AIzaSyB9YNikK_nFhURlOxMGN-f2CiqpYx9_2is'
-};
